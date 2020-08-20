4327560dbc9c90b0e3918b752ae275b6bc84a2c1
alexander <vampirwot@yandex.ru>
2020-08-20 13:43:08 +0300

    9 01 HW8

--- src/main/webapp/resources/js/topjava.meals.js
+++ src/main/webapp/resources/js/topjava.meals.js
@@ -1,6 +1,43 @@
-$(function (){
+function updateFilteredTable() {
+    $.ajax({
+        type: "GET",
+        url: "profile/meals/filter",
+        data: $("#filter").serialize()
+    }).done(updateTableByData);
+}
+
+$(function () {
     makeEditable({
-        ajaxUrl: "profile/meals/"
-    }
-);
-})
+        ajaxUrl: "profile/meals/",
+        datatableApi: $("#datatable").DataTable({
+            "paging": false,
+            "info": true,
+            "columns": [
+                {
+                    "data": "dateTime"
+                },
+                {
+                    "data": "description"
+                },
+                {
+                    "data": "calories"
+                },
+                {
+                    "defaultContent": "Edit",
+                    "orderable": false
+                },
+                {
+                    "defaultContent": "Delete",
+                    "orderable": false
+                }
+            ],
+            "order": [
+                [
+                    0,
+                    "desc"
+                ]
+            ]
+        }),
+        updateTable: updateFilteredTable
+    });
+});
